e86a257a95016072ca292ec19a706edaaa3dd510
芋道源码 <zhijiantianya@gmail.com>
2023-04-02 14:48:43 +0000

    回退 'Pull Request !84 : 修改测试所提bug'

--- src/views/infra/codegen/PreviewCode.vue
+++ src/views/infra/codegen/PreviewCode.vue
@@ -170,11 +170,11 @@ const handleFiles = (datas: CodegenApi.CodegenPreviewVO[]) => {
 /** 复制 **/
 const copy = async (text: string) => {
   const { copy, copied, isSupported } = useClipboard({ source: text })
-  if (!isSupported.value) {
+  if (!isSupported) {
     message.error(t('common.copyError'))
   } else {
     await copy()
-    if (unref(copied.value)) {
+    if (unref(copied)) {
       message.success(t('common.copySuccess'))
     }
   }
