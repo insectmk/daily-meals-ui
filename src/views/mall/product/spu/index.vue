437cbd66271e3419a6a57c9dc54da42a087f24bc
puhui999 <puhui999@163.com>
2023-05-31 15:37:26 +0800

    fix: 完善 SPU 选择快递模版的功能

--- src/views/mall/product/spu/index.vue
+++ src/views/mall/product/spu/index.vue
@@ -410,7 +410,7 @@ const openForm = (id?: number) => {
  * 查看商品详情
  */
 const openDetail = (id?: number) => {
-  push('/product/productSpuDetail' + id)
+  push('/product/productSpuDetail/' + id)
 }
 
 /** 导出按钮操作 */
