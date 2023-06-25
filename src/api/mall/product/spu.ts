c56f39f953f8d54b9cc921aa394a9ff047c38351
puhui999 <puhui999@163.com>
2023-06-25 16:43:49 +0800

    fix: 封装 SpuSelect SpuAndSkuList 为商品活动商品选择商品编辑通用组件

--- src/api/mall/product/spu.ts
+++ src/api/mall/product/spu.ts
@@ -16,22 +16,36 @@ export function updateSpu(data) {
   })
 }
 
-// 删除商品 SPU
-export function deleteSpu(id) {
-  return request.delete({
-    url: `/product/spu/delete?id=${id}`
-  })
-}
-
-// TODO @puhui999: SpuRespVO 合并到 SPU 里？前端少点 VO 类哈；
-export interface SpuRespVO extends Spu {
-  price: number
-  salesCount: number
-  marketPrice: number
-  costPrice: number
-  stock: number
-  createTime: Date
-  status: number
+export interface Spu {
+  id?: number
+  name?: string // 商品名称
+  categoryId?: number | null // 商品分类
+  keyword?: string // 关键字
+  unit?: number | null // 单位
+  picUrl?: string // 商品封面图
+  sliderPicUrls?: string[] // 商品轮播图
+  introduction?: string // 商品简介
+  deliveryTemplateId?: number | null // 运费模版
+  brandId?: number | null // 商品品牌编号
+  specType?: boolean // 商品规格
+  subCommissionType?: boolean // 分销类型
+  skus?: Sku[] // sku数组
+  description?: string // 商品详情
+  sort?: number // 商品排序
+  giveIntegral?: number // 赠送积分
+  virtualSalesCount?: number // 虚拟销量
+  recommendHot?: boolean // 是否热卖
+  recommendBenefit?: boolean // 是否优惠
+  recommendBest?: boolean // 是否精品
+  recommendNew?: boolean // 是否新品
+  recommendGood?: boolean // 是否优品
+  price?: number // 商品价格
+  salesCount?: number // 商品销量
+  marketPrice?: number // 市场价
+  costPrice?: number // 成本价
+  stock?: number // 商品库存
+  createTime?: Date // 商品创建时间
+  status?: number // 商品状态
 }
 
 // 获得 Spu 列表
