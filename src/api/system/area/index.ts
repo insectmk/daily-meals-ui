397c9baa0662a23d4b6f4b59d9ceef83566dd17b
YunaiV <zhijiantianya@gmail.com>
2023-10-02 18:30:22 +0800

    trade：优化运费模版的实现

--- src/api/system/area/index.ts
+++ src/api/system/area/index.ts
@@ -5,13 +5,6 @@ export const getAreaTree = async () => {
   return await request.get({ url: '/system/area/tree' })
 }
 
-export const getChildrenArea = async (id: number) => {
-  return await request.get({ url: '/system/area/get-children?id=' + id })
-}
-
-export const getAreaListByIds = async (ids) => {
-  return await request.get({ url: '/system/area/get-by-ids?ids=' + ids })
-}
 // 获得 IP 对应的地区名
 export const getAreaByIp = async (ip) => {
   return await request.get({
