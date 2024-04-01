477a0b4aadd668792e92e280d5d40f6ccabfa1f6
AhJindeg <AhJindeg@gmail.com>
2024-04-01 16:14:30 +0800

    fix(role): 🐞 add nextTick for open
    - 增加 nextTick()，修复在权限范围为指定机构并且已选择数据时，编辑打开提示 setChecked undefined 问题

--- src/views/system/role/RoleDataPermissionForm.vue
+++ src/views/system/role/RoleDataPermissionForm.vue
@@ -102,11 +102,10 @@ const open = async (row: RoleApi.RoleVO) => {
   formData.name = row.name
   formData.code = row.code
   formData.dataScope = row.dataScope
-
-  await nextTick()
-  // 需要在 DOM 渲染完成后，再设置选中状态
-  row.dataScopeDeptIds?.forEach((deptId: number) => {
-    treeRef.value.setChecked(deptId, true, false)
+  nextTick((): void => {
+    row.dataScopeDeptIds?.forEach((deptId: number) => {
+      treeRef.value.setChecked(deptId, true, false)
+    })
   })
 }
 defineExpose({ open }) // 提供 open 方法，用于打开弹窗
