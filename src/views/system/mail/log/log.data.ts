6aa1bcaaeca5ab29f0d2ae63dc95fcc6529e53cd
芋道源码 <zhijiantianya@gmail.com>
2024-08-21 13:41:32 +0000

    回退 'Pull Request !505 : 【修复】el-input、el-select、el-date-picker宽度；el-radio的label属性兼容下个版本；优化iframe布局'

--- src/views/system/mail/log/log.data.ts
+++ src/views/system/mail/log/log.data.ts
@@ -21,10 +21,7 @@ const crudSchemas = reactive<CrudSchema[]>([
       componentProps: {
         valueFormat: 'YYYY-MM-DD HH:mm:ss',
         type: 'daterange',
-        defaultTime: [new Date('1 00:00:00'), new Date('1 23:59:59')],
-        style: {
-          width: '240px'
-        }
+        defaultTime: [new Date('1 00:00:00'), new Date('1 23:59:59')]
       }
     },
     {
@@ -131,14 +128,7 @@ const crudSchemas = reactive<CrudSchema[]>([
     label: '用户编号',
     field: 'userId',
     isSearch: true,
-    isTable: false,
-    search: {
-      componentProps: {
-        style: {
-          width: '240px'
-        }
-      }
-    }
+    isTable: false
   },
   {
     label: '用户类型',
@@ -146,14 +136,7 @@ const crudSchemas = reactive<CrudSchema[]>([
     dictType: DICT_TYPE.USER_TYPE,
     dictClass: 'number',
     isSearch: true,
-    isTable: false,
-    search: {
-      componentProps: {
-        style: {
-          width: '240px'
-        }
-      }
-    }
+    isTable: false
   },
   {
     label: '邮件标题',
@@ -174,14 +157,7 @@ const crudSchemas = reactive<CrudSchema[]>([
     field: 'sendStatus',
     dictType: DICT_TYPE.SYSTEM_MAIL_SEND_STATUS,
     dictClass: 'string',
-    isSearch: true,
-    search: {
-      componentProps: {
-        style: {
-          width: '240px'
-        }
-      }
-    }
+    isSearch: true
   },
   {
     label: '邮箱账号',
@@ -195,9 +171,6 @@ const crudSchemas = reactive<CrudSchema[]>([
         optionsAlias: {
           labelField: 'mail',
           valueField: 'id'
-        },
-        style: {
-          width: '240px'
         }
       }
     }
@@ -212,14 +185,7 @@ const crudSchemas = reactive<CrudSchema[]>([
   {
     label: '模板编号',
     field: 'templateId',
-    isSearch: true,
-    search: {
-      componentProps: {
-        style: {
-          width: '240px'
-        }
-      }
-    }
+    isSearch: true
   },
   {
     label: '模板编码',
