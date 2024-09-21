7ede9b9041ca249da11c412e43413f040fb50d6f
安浩浩 <1036606149@qq.com>
2024-09-21 09:15:27 +0800

    【新增】 IOT 设备管理

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -627,7 +627,7 @@ const remainingRouter: AppRouteRecordRaw[] = [
     children: [
       {
         path: 'product/detail/:id',
-        name: 'IotProductDetail',
+        name: 'IoTProductDetail',
         meta: {
           title: '产品详情',
           noCache: true,
