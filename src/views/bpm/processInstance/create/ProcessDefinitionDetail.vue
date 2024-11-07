1af368df26bf97293e828f4826e5628cdecadc23
GoldenZqqq <1361001127@qq.com>
2024-11-07 13:49:18 +0800

    fix: 流程发起页面-修复重新发起时表单数据未回显问题

--- src/views/bpm/processInstance/create/ProcessDefinitionDetail.vue
+++ src/views/bpm/processInstance/create/ProcessDefinitionDetail.vue
@@ -176,7 +176,7 @@ const getApprovalDetail = async (row: any) => {
 }
 
 /** 提交按钮 */
-const submitForm = async (formData: any) => {
+const submitForm = async () => {
   if (!fApi.value || !props.selectProcessDefinition) {
     return
   }
@@ -196,7 +196,7 @@ const submitForm = async (formData: any) => {
   try {
     await ProcessInstanceApi.createProcessInstance({
       processDefinitionId: props.selectProcessDefinition.id,
-      variables: formData || detailForm.value.value,
+      variables: detailForm.value.value,
       startUserSelectAssignees: startUserSelectAssignees.value
     })
     // 提示
