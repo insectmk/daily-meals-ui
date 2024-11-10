0951fa4f8fe22c3184cabf057c3a7357970fcd9b
puhui999 <puhui999@163.com>
2024-11-10 14:15:15 +0800

    【功能完善】商城: 客服会员信息

--- src/views/member/user/detail/UserAccountInfo.vue
+++ src/views/member/user/detail/UserAccountInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-descriptions :column="2">
+  <el-descriptions :column="column">
     <el-descriptions-item>
       <template #label>
         <descriptions-item-label label=" 等级 " icon="svg-icon:member_level" />
@@ -50,7 +50,10 @@ import * as UserApi from '@/api/member/user'
 import * as WalletApi from '@/api/pay/wallet/balance'
 import { UserTypeEnum } from '@/utils/constants'
 import { fenToYuan } from '@/utils'
-const props = defineProps<{ user: UserApi.UserVO; wallet: WalletApi.WalletVO }>() // 用户信息
+
+withDefaults(defineProps<{ user: UserApi.UserVO; wallet: WalletApi.WalletVO; column?: number }>(), {
+  column: 2
+}) // 用户信息
 </script>
 <style scoped lang="scss">
 .cell-item {
