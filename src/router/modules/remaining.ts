dd169b73d4c20c0f3130a675d7ca53164102446d
YunaiV <zhijiantianya@gmail.com>
2025-01-28 22:29:54 +0800

    【代码评审】IoT：物模型的管理

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -667,7 +667,7 @@ const remainingRouter: AppRouteRecordRaw[] = [
           title: '产品详情',
           noCache: true,
           hidden: true,
-          activeMenu: '/iot/product'
+          activeMenu: '/iot/device/product'
         },
         component: () => import('@/views/iot/product/product/detail/index.vue')
       },
