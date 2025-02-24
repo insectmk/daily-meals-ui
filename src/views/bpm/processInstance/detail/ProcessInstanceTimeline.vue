7e0b805689d9acffc41c6d72bcc5ac27624be814
Lesan <1960681385@qq.com>
2025-02-24 09:40:52 +0800

    fix: 代码审查修改

--- src/views/bpm/processInstance/detail/ProcessInstanceTimeline.vue
+++ src/views/bpm/processInstance/detail/ProcessInstanceTimeline.vue
@@ -181,7 +181,6 @@ import parallelSvg from '@/assets/svgs/bpm/parallel.svg'
 import finishSvg from '@/assets/svgs/bpm/finish.svg'
 import transactorSvg from '@/assets/svgs/bpm/transactor.svg'
 import childProcessSvg from '@/assets/svgs/bpm/child-process.svg'
-import asyncChildProcessSvg from '@/assets/svgs/bpm/async-child-process.svg'
 
 defineOptions({ name: 'BpmProcessInstanceTimeline' })
 withDefaults(
@@ -253,8 +252,6 @@ const nodeTypeSvgMap = {
   [NodeType.PARALLEL_BRANCH_NODE]: { color: '#14bb83', svg: parallelSvg },
   // 子流程节点
   [NodeType.CHILD_PROCESS_NODE]: { color: '#14bb83', svg: childProcessSvg },
-  // 异步子流程节点
-  [NodeType.ASYNC_CHILD_PROCESS_NODE]: { color: '#14bb83', svg: asyncChildProcessSvg }
 }
 
 // 只有只有状态是 -1、0、1 才展示头像右小角状态小icon
@@ -275,7 +272,6 @@ const getApprovalNodeIcon = (taskStatus: number, nodeType: NodeType) => {
     nodeType === NodeType.USER_TASK_NODE ||
     nodeType === NodeType.TRANSACTOR_NODE ||
     nodeType === NodeType.CHILD_PROCESS_NODE ||
-    nodeType === NodeType.ASYNC_CHILD_PROCESS_NODE ||
     nodeType === NodeType.END_EVENT_NODE
   ) {
     return statusIconMap[taskStatus]?.icon
