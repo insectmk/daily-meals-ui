cf7b59482fb050429aa5ee69c64589e66d358026
InsectMk <makun_ing@126.com>
2025-04-25 14:46:20 +0800

    chore: 删除更改为vben模板后的无效代码

--- git-cliff.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Generating CHANGELOG.md...
-git cliff -o CHANGELOG.md
-if %errorlevel% equ 0 (
-    echo CHANGELOG.md generated successfully.
-) else (
-    echo Failed to generate CHANGELOG.md.
-)
-pause
